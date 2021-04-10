51cca9f8f3aaf3848a5df716099b2f327836f260
Divyang Mittal <divyangmittal44@gmail.com>
2021-04-10 16:49:09 +0530

    Add user scripts and changes in network params Ports for all the user scripts have been added in network params

--- master/messaging/network_params.py
+++ master/messaging/network_params.py
@@ -3,11 +3,16 @@
 CLIENT_RECV_PORT = 5005
 CLIENT_SEND_PORT = 5006
 
+SUBMIT_RECV_PORT = 1001
+STATS_RECV_PORT = 1002
+KILL_RECV_PORT = 1003
+
 # Buffer size for socket
 BUFFER_SIZE = 1048576
 
 # IP to be used for local system
-LOCAL_IP = '127.0.0.1'
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 8081
 
 # Time (in sec) after which it's assumed that a communicating node has crashed
 CRASH_ASSUMPTION_TIME = 10
